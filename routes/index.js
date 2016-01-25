29b045ab8825733543c84cded29bffdbfb94aea2
mudcake <gina.lau@gmail.com>
2016-01-25 15:32:21 +1100

    comments not working

--- routes/index.js
+++ routes/index.js
@@ -43,4 +43,52 @@ router.get('/posts/:post', function(req, res) {
   res.json(req.post);
 });
 
+router.put('/posts/:post/upvote', function(req, res, next) {
+  req.post.upvote(function(err, post) {
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
+});
+
+router.param('comment', function(req, res, next, id) {
+  var query = Comment.findById(id);
+
+  query.exec(function (err, comment) {
+    if (err) { return next(err); }
+    if (!comment) { return next(new Error('can\'t find comment')); }
+
+    req.comment = comment;
+    return next();
+  });
+});
+
+router.get('/posts/:post/comments', function(req, res, next) {
+  res.json(req.post.comments);
+});
+
+router.post('/posts/:post/comments', function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.post = req.body;
+
+  comment.save(function(err, comment){
+    if(err){ return next(err); }
+
+    req.post.comments.push(comment);
+    req.post.save(function(err, post) {
+      if(err){ return next(err); }
+
+      res.json(comment);
+    });
+  });
+});
+
+router.post('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+  req.comment.upvote(function (err, comment) {
+    if (err) { return next(err); }
+
+    res.json(comment);
+  });
+});
+
 module.exports = router;
